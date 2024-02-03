4f6e2a13d9f4e0f053c90e868414c5cf200b9b32
ishwarb13 <ishwarb1303@gmail.com>
2024-02-03 18:40:30 +0000

    app

--- app.py
+++ app.py
@@ -43,12 +43,14 @@ def predict():
 
     return render_template('result.html', prediction_text=f'Customer Segment: {output}')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
 
 
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
